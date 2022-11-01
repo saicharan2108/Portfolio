877eb633d231c7182a6ef74f0a2e07a5d774b022
saivaraprasad@48 <saivara@12345>
2022-11-01 21:39:07 +0530

    ! removed from projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,7 +4,7 @@ import Header from "..//Header/index"
 const Projects = () => (
     <div className="header" style={{height:"200vh"}}>
         <Header/>
-        <h1> Hello Projects! </h1>
+        <h1> Hello Projects </h1>
     </div>
 )
 
