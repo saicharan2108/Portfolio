eab056d82fa4846b4657b1bc8455e531bd7f639f
saivaraprasad@48 <saivara@12345>
2022-11-10 21:27:51 +0530

    green added

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     </div>
       <div className="mobile-info">
         <img src="https://assets.ccbp.in/frontend/react-js/failure-img.png" alt="img" className="failure-img" />
-        <h1 className="txt"> Unavailable for<b><i> <span style={{color:"red"}}>Mobile Devices</span></i></b>, Make sure to explore on <u>Laptop or Tablet</u>.</h1>
+        <h1 className="txt"> Unavailable for<b><i> <span style={{ color: "red" }}>Mobile Devices</span></i></b>, Make sure to explore on <u><span style={{color:"green"}}>Laptop or Tablet</span></u>.</h1>
       </div>
       </>
   );
